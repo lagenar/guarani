223c09747585046e92f5d6c061c3573b3a07d06c
Lucas Moauro <lagenar@gmail.com>
2010-07-27 13:45:37 -0300

    Guarani

--- guarani.py
+++ guarani.py
@@ -2,25 +2,40 @@ from mechanize import Browser
 from BeautifulSoup import BeautifulSoup
 import subprocess
 
-USUARIO, CLAVE = ("usuario", "clave")
-br = Browser()
-br.open("https://guarani.exa.unicen.edu.ar/Guarani3w/")
-br.open("https://guarani.exa.unicen.edu.ar/Guarani3w/includes/barra.inc.php")
-br.follow_link(text_regex="Iniciar")
-br.select_form(nr=0)
-br["fUsuario"] = USUARIO
-br["fClave"] = CLAVE
-br.submit()
-br.open("https://guarani.exa.unicen.edu.ar/Guarani3w/operaciones.php")
-br.follow_link(url_regex="consultarActProvisoria")
-r = br.response()
-soup = BeautifulSoup(''.join(r.readlines()))
-s_notas = soup.findAll('tr', {'class' : 'normal'})
-notas = []
-for s_nota in s_notas:
-    materia, nota = [x.text for x in s_nota.findAll('td')[:2]]
-    notas.append([materia, nota])
+class Guarani(object):
+    def __init__(self, user, passwd):
+        self.br = Browser()
+        self.user = user
+        self.passwd = passwd
+        self._login()
 
-for nota in notas:
-    if nota[1] != '':
+    def _login(self):
+        self.br.open("https://guarani.exa.unicen.edu.ar/Guarani3w/")
+        self.br.open("https://guarani.exa.unicen.edu.ar/Guarani3w/includes/barra.inc.php")
+        self.br.follow_link(text_regex="Iniciar")
+        self.br.select_form(nr=0)
+        self.br["fUsuario"] = self.user
+        self.br["fClave"] = self.passwd
+        self.br.submit()
+
+    def _parseNotas(self, html):
+        soup = BeautifulSoup(html)
+        s_notas = soup.findAll('tr', {'class' : 'normal'})
+        notas = []
+        for s_nota in s_notas:
+            materia, nota = [x.text for x in s_nota.findAll('td')[:2]]
+            if nota != '':
+                notas.append([materia, nota])
+        return notas
+    
+    def getNotasFinales(self):
+        self.br.open("https://guarani.exa.unicen.edu.ar/Guarani3w/operaciones.php")
+        self.br.follow_link(url_regex="consultarActProvisoria")
+        return self._parseNotas(self.br.response().read())
+
+if __name__ == "__main__":
+    import config
+    g = Guarani(config.usuario, config.clave)
+    notas = g.getNotasFinales()
+    for nota in notas:
         subprocess.Popen(['notify-send', '-t', '0', ' '.join(nota)])
